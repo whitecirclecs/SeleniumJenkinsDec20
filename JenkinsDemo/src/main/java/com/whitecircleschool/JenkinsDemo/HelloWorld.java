5bf91abf2e8359ff132ac102cf7b94be1ff79780
whitecirclecs <deepinder@whitecircleschool.com>
2018-12-20 23:12:45 -0500

    Source Code for Jenkins

---
+++ JenkinsDemo/src/main/java/com/whitecircleschool/JenkinsDemo/HelloWorld.java
@@ -0,0 +1,34 @@
+package com.whitecircleschool.JenkinsDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+	
+	WebDriver driver;
+
+	@BeforeTest
+	public void openBrowser() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://whitecircleschool.com/selenium-3-0-web-automation-testing/");
+	}
+
+	@Test
+	public void titleVerification() {
+		String expectedTitle = "Selenium 3.0 Web-Automation Testing";
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle, expectedTitle);
+	}
+
+	@AfterTest
+	public void closeBrowser() {
+		driver.close();
+	}
+
+}
